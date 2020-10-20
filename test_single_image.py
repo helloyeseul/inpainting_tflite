4e441675ed7a50cfe60a551e8c870969f8edf6a9
yeseul <sdc01194@gmail.com>
2020-10-20 17:23:56 +0900

    refactor test_single_image.py

--- test_single_image.py
+++ test_single_image.py
@@ -9,118 +9,122 @@ import neuralgym as ng
 from inpaint_model import InpaintCAModel
 
 
-def test_single_image(image, mask, out,
-                      image_height, image_width,
-                      checkpoint_dir):
+def get_dirs(data_dir, index):
+    # prepare directories
+    input_dir = data_dir + "/input"
+    mask_dir = data_dir + "/mask"
+    output_dir = data_dir + "/output"
+
+    # check output directory exists
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    dir_files = os.listdir(input_dir)
+    dir_files.sort()
+
+    base = os.path.basename(dir_files[index])
+
+    image = input_dir + "/" + base
+    mask = mask_dir + "/" + base
+    out = output_dir + "/" + base
+
+    return image, mask, out
+
+
+def get_input(image_path, mask_path, image_width, image_height):
+    # load image and mask
+    image = cv2.imread(image_path)
+    mask = cv2.imread(mask_path)
+
+    # resize image
+    image = cv2.resize(image, (image_width, image_height))
+    mask = cv2.resize(mask, (image_width, image_height))
+
+    assert image.shape == mask.shape
+
+    h, w, _ = image.shape
+    grid = 8
+    image = image[: h // grid * grid, : w // grid * grid, :]
+    mask = mask[: h // grid * grid, : w // grid * grid, :]
+
+    print("image shape: {}".format(image.shape))
+
+    # reshape image and mask : [h, w, 3] to [1, h, w, 3]
+    image = np.expand_dims(image, 0)
+    mask = np.expand_dims(mask, 0)
+
+    # concat [image]+[mask] to make input image
+    # input_image shape : [1, h, w * 2, 3]
+    input_image = np.concatenate([image, mask], axis=2)
+
+    print("input image shape: {}".format(input_image.shape))
+
+    return input_image, h, w
+
+
+def test_single_image(input_image, output_path, image_height, image_width, ckpt_dir):
 
     # start sess configuration
     sess_config = tf.ConfigProto()
     sess_config.gpu_options.allow_growth = True
     sess = tf.Session(config=sess_config)
 
+    # inpaint model
     model = InpaintCAModel()
 
     # input node placeholder
-    input_image_ph = tf.placeholder(                                                                                                                                                                                                          
-        tf.float32, name='input', shape=(1, image_height, image_width*2, 3)) 
+    input_image_ph = tf.placeholder(
+        tf.float32, name="input", shape=(1, image_height, image_width * 2, 3)
+    )
 
-    output = model.build_server_graph(input_image_ph)                                                                                                                                                                                         
-    output = (output + 1.) * 127.5                                                                                                                                                                                                            
-    output = tf.reverse(output, [-1])                                                                                                                                                                                                         
+    output = model.build_server_graph(input_image_ph)
+    output = (output + 1.0) * 127.5
+    output = tf.reverse(output, [-1])
     output = tf.saturate_cast(output, tf.uint8)
-    
-    vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)                                                                                                                                                                              
+
+    # load variables from checkpoint
+    vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
     assign_ops = []
-    for var in vars_list:                                                                                                                                                                                                                     
-        vname = var.name                                                                                                                                                                                                                      
-        from_name = vname                                                                                                                                                                                                                     
-        var_value = tf.contrib.framework.load_variable(                                                                                                                                                                                       
-            args.checkpoint_dir, from_name)                                                                                                                                                                                                   
+    for var in vars_list:
+        vname = var.name
+        from_name = vname
+        var_value = tf.contrib.framework.load_variable(ckpt_dir, from_name)
         assign_ops.append(tf.assign(var, var_value))
-    
-    sess.run(assign_ops)     
-    
-    # load image
-    image = cv2.imread(image)
-    mask = cv2.imread(mask)
 
-    # resize image
-    image = cv2.resize(image, (args.image_width, args.image_height))                                                                                                                                                                      
-    mask = cv2.resize(mask, (args.image_width, args.image_height)) 
-    
-    assert image.shape == mask.shape
+    sess.run(assign_ops)
 
-    h, w, _ = image.shape                                                                                                                                                                                                                 
-    grid = 8                                                                                                                                                                                                                              
-    image = image[:h//grid*grid, :w//grid*grid, :]                                                                                                                                                                                        
-    mask = mask[:h//grid*grid, :w//grid*grid, :]                                                                                                                                                                                          
-    print('image shape: {}'.format(image.shape))
+    # run model
+    print("Running model ...")
+    result = sess.run(output, feed_dict={input_image_ph: input_image})
+    print("Running model done.")
 
-    # [512, 680, 3] to [1, 512, 680, 3]
-    image = np.expand_dims(image, 0)                                                                                                                                                                                                      
-    mask = np.expand_dims(mask, 0)
+    # save output image to file
+    print("Saving output to {}".format(output_path))
+    cv2.imwrite(output_path, result[0][:, :, ::-1])
+    print("Saving output done.")
 
-    # [image]+[mask] to [1, 512, 1360, 3]
-    input_image = np.concatenate([image, mask], axis=2)
-    print('input_image [image + mask] shape: {}'.format(input_image.shape))
+    # save checkpoint
+    saver = tf.train.Saver().save(sess, "./model_logs/test/model.ckpt")
 
-    # run pretrained model                                                                                                                                                                                                               
-    result = sess.run(output, feed_dict={input_image_ph: input_image})                                                                                                                                                                    
-    print('Processed: {}'.format(out))                                                                                                                                                             
-    cv2.imwrite(out, result[0][:, :, ::-1])
+    # save graph for Tensorboard
+    tf.summary.FileWriter("./tbgraph", sess.graph)
 
-    print('Processing {} done.'.format(out))
-
-    # 체크포인트 저장
-    saver = tf.train.Saver().save(sess, './model_logs/test/model.ckpt')
-
-    # 그래프 저장
+    # save graph to file
     # tf.train.write_graph(sess.graph_def, './graph/', 'graph.pbtxt')
     # tf.train.write_graph(sess.graph_def, './graph/', 'graph.pb')
-    
-    # tensorboard 출력을 위한 그래프 저장
-    # tf.summary.FileWriter('./tbgraph', sess.graph)
 
 
-    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--test_dir', default='', type=str,
-                        help='The directory of test images to be completed.')
-    parser.add_argument('--image_height', default='', type=int,
-                        help='The height of test images')
-    parser.add_argument('--image_width', default='', type=int,
-                        help='The width of test images')
-    parser.add_argument('--checkpoint_dir', default='', type=str,
-                        help='The directory of tensorflow checkpoint.')
-    
+
+    parser.add_argument("--data_dir", default="", type=str)
+    parser.add_argument("--image_height", default="", type=int)
+    parser.add_argument("--image_width", default="", type=int)
+    parser.add_argument("--ckpt_dir", default="", type=str)
+
     args = parser.parse_args()
 
-    # prepare folder
-    input_folder = args.test_dir + "/input"
-    mask_folder = args.test_dir + "/mask"
-    output_folder = args.test_dir + "/output"
+    image, mask, out = get_dirs(args.data_dir, 0)
+    input_image, h, w = get_input(image, mask, args.image_width, args.image_height)
 
-    # check output dir 
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
-    
-    dir_files = os.listdir(input_folder)
-    dir_files.sort()
-    
-    file_inter = dir_files[0]
-    
-    base = os.path.basename(file_inter)
-
-    image = input_folder + "/" + base
-    mask = mask_folder + "/" + base
-    out = output_folder + "/" + base
-
-    test_single_image(image, mask, out,
-                      args.image_height,
-                      args.image_width,
-                      args.checkpoint_dir)
-
-    
-        
+    test_single_image(input_image, out, h, w, args.ckpt_dir)
